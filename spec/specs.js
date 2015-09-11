23a213a7e17fb151514471709c12996eb1c11c6c
Tim White <fignewtonfood@gmail.com>
2015-09-11 07:45:41 -0700

    pass first seven specs

--- spec/specs.js
+++ spec/specs.js
@@ -7,4 +7,27 @@ describe('replace function tests', function() {
         expect(replacement("a","a","b")).to.equal("b");
     });
 
+    it("replace one word with another", function () {
+        expect(replacement("anteater","anteater","bear")).to.equal("bear");
+    });
+
+    it("replace one word in string with another word", function () {
+        expect(replacement("Anteater sleeps.","Anteater","Bear")).to.equal("Bear sleeps.");
+    });
+
+    it("replace one word with multiple words", function () {
+        expect(replacement("Anteater sleeps.","sleeps","snoozes and dreams")).to.equal("Anteater snoozes and dreams.");
+    });
+
+    it("replace multiple words with one word", function () {
+        expect(replacement("Anteater sleeps.","Anteater sleeps.","Wow!")).to.equal("Wow!");
+    });
+
+    it("replace multiple words with multiple words", function () {
+        expect(replacement("Anteater sleeps.","Anteater sleeps","Bear grills")).to.equal("Bear grills.");
+    });
+
+    it("replace multiple instances", function () {
+        expect(replacement("anteater and anteater", "anteater", "bear")).to.equal("bear and bear");
+    });
 });
